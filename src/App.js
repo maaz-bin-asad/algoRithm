347134151e31864ed3c73952651905dddb3832df
Maaz <maazbinasad29@gmail.com>
2020-05-16 05:28:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
-function func(){
+function func() {
   console.log("I worked")
 }
-let user={
-  first:"Maaz"
+let user = {
+  first: "Maaz"
 }
-class App extends Component{       
-  render(){
-    return <React.Fragment>
-      <h4 className="elem">I am {func()}</h4> 
+document.getElementById('button').addEventListener('click',(event)=>{
+  console.log(" I have been trigered by the newly created element")
+  console.log(event.clientX)
+})
 
-  <p>Hey I am {this.props.key}</p>
+let arr = [1, 2, 3]
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return <React.Fragment>
+      <h4 className="elem">I am {func()}</h4>
+
+      <p>Hey I am {this.props.hey}</p>
+      <p>Hey I am being written</p>
+      <p>Hey I am a {user.first}</p>
+      <h1>Hey I am a new {arr[1]}</h1>
+      <p>I would like to call this function {document.getElementById('root').addEventListener('click', (e)=>{
+        console.log("hey i am clicked")
+        console.log(e.target)
+      })}</p>
     </React.Fragment>
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 ReactDOM.render(
-  <App key="value" />,
+  <App hey="name" />,
   document.getElementById('root')
 );
+var b;
+b=7
+c=9;
+var c;
+let d=19;
+function func(){
+    d=8
+}
+func()
+console.log(d)
+console.log(b)
+if(true){
+  var a=5;
+}
+console.log(a)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

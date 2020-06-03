735c60149025097faeddd441c4200051e08299bd
Maaz <maazbinasad29@gmail.com>
2020-06-04 03:00:07 +0530

    Add Context API

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { SORTING } from './shared/sorting'
 import Footer from './Footer'
 import ThemeProvider from './context/Theme'
 import Temp from './Float'
+import AuthContextProvider from './context/AuthContext'
 
 const App=()=>{
     const [algo,setName]=useState({
@@ -29,9 +30,12 @@ const App=()=>{
     <h2>This is a website that contains several algorithms in a way that even a beginner understands and realizes the importance of studying algorithms</h2>
     <hr />
     <ThemeProvider>
+        <AuthContextProvider>
         <Temp />
         <Toggle />
+        </AuthContextProvider>
         </ThemeProvider>
+        
 <Header />
 <Switch>
     <Route path='/home' component={Home} />

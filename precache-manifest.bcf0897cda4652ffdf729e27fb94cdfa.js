4d32ac084aa6e4823086858793624351d95c0a41
Maaz <maazbinasad29@gmail.com>
2020-06-05 01:35:39 +0530

    Updates

---
+++ precache-manifest.bcf0897cda4652ffdf729e27fb94cdfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9216087ceb13617ed3a894e73a4683d6",
+    "url": "/practical-react/index.html"
+  },
+  {
+    "revision": "fa5f9e33a04d4cfb521b",
+    "url": "/practical-react/static/js/2.547f0212.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/practical-react/static/js/2.547f0212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3fe3f331029fe8a981e",
+    "url": "/practical-react/static/js/main.b746192c.chunk.js"
+  },
+  {
+    "revision": "fd4a3bda6bb3a0453b1d",
+    "url": "/practical-react/static/js/runtime-main.8737ee52.js"
+  }
+]);
